544e473c86a642bd2847c6659b7016fa6c93c46e
Shravan Chouti <schouti@cisco.com>
2021-02-04 18:54:04 +0530

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   const [currentSum, setCurrentSum] = useState(0);
@@ -18,7 +23,7 @@ function App() {
     e.preventDefault();
     if (clear) setClear(false);
     let currentNum = document.querySelector('#num').value
-    if (currentNum == '')
+    if (currentNum === '')
       return;
     let sum = currentSum + parseInt(currentNum);
     setCurrentSum(sum);
@@ -28,24 +33,77 @@ function App() {
 
   const Clear = (e) => {
     e.preventDefault();
-    console.log('sum:', currentSum);
     document.querySelector('form').reset();
     setClear(true);
     setCurrentSum(0);
   }
-  return (
-    <div className="App">
-      <div className="app-title">
-        <h1> Basic Form Calculator for Addition</h1>
+
+  function Home() {
+    return (
+      <div>
+        <h2>Home</h2>
       </div>
-      <form>
-        <input type="text" id="result" value={currentSum} readOnly />
-        <input type="text" id="num" placeholder="enter a number" />
-        <button onClick={Add}>Add</button>
-        <button onClick={Clear}>Clear</button>
-      </form>
+    );
+  }
+
+  function About() {
+    return (
+      <div>
+        <h2>About</h2>
+      </div>
+    );
+  }
+
+  function Dashboard() {
+    return (
+      <div>
+        <h2>Dashboard</h2>
+      </div>
+    );
+  }
+  return (
+
+    <div className="App">
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+             
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+          <hr />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      <div className="app-title">
+                <h1> Basic Form Calculator for Addition</h1>
+              </div>
+              <form>
+                <input type="text" id="result" value={currentSum} readOnly />
+                <input type="text" id="num" placeholder="enter a number" />
+                <button onClick={Add}>Add</button>
+                <button onClick={Clear}>Clear</button>
+              </form>
     </div>
   )
+
 }
 
 export default App;
